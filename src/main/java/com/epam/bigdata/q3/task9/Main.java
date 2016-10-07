14ee020ca318c34ab88a530391712c7f2563fc99
Maryna Maroz <maryna_maroz@epam.com>
2016-10-07 18:15:01 +0300

    add main

--- src/main/java/com/epam/bigdata/q3/task9/Main.java
+++ src/main/java/com/epam/bigdata/q3/task9/Main.java
@@ -1,13 +1,22 @@
 package com.epam.bigdata.q3.task9;
 
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
-public class Main 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class Main {
+	 public static void main(String[] args) throws IOException {
+	        if (args.length < 1) {
+	            throw new IllegalArgumentException("Must have either 'producer' or 'consumer' as argument");
+	        }
+	        switch (args[0]) {
+	            case "producer":
+	                Producer.main(args);
+	                break;
+	            default:
+	                throw new IllegalArgumentException("Don't know how to do " + args[0]);
+	        }
+	    }
 }
